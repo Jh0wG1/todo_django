1dfea17bb28555ca48cd9d3dd82883dfa3517646
Jhow <jhones.gasparinidev@gmail.com>
2021-11-19 20:09:41 -0300

    dados em tela

--- tarefa/views.py
+++ tarefa/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import TarefaBD
 
 # Create your views here.
+def index(request):
+    conteudo = TarefaBD.objects.all()
+    context ={
+        'conteudos':conteudo
+    }
+    return render(request,'lista.html',context)
